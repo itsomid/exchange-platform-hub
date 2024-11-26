14569657d23668b6e7740c77e12297c2fe95407a
OMID-PC\Omid <o.shabani@hotmail.com>
2024-11-26 10:15:16 +0330

    fix seed issue

--- admin-panel/database/seeders/DatabaseSeeder.php
+++ admin-panel/database/seeders/DatabaseSeeder.php
@@ -33,8 +33,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminTableSeeder::class,
-            ReferralCodeSeeder::class,
             UserTableSeeder::class,
+            ReferralCodeSeeder::class,
             CurrenciesSeeder::class,
             TransactionSeeder::class,
             SettingTableSeeder::class,
