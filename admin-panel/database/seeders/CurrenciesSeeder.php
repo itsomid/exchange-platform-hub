0c75f1f297cd185889efc63293acd311c4df77fe
OMID-PC\Omid <o.shabani@hotmail.com>
2024-11-26 10:06:27 +0330

    remove unique from field to support usdt

--- admin-panel/database/seeders/CurrenciesSeeder.php
+++ admin-panel/database/seeders/CurrenciesSeeder.php
@@ -40,7 +40,7 @@ class CurrenciesSeeder extends Seeder
             [
                 'type' => 'erc20', // Ethereum token (ERC20)
                 'name' => 'Tether',
-                'symbol' => '₮ (ERC20)',
+                'symbol' => '₮',
                 'code' => 'USDT',
                 'logo' => 'usdt.svg',
                 'is_active' => '1',
