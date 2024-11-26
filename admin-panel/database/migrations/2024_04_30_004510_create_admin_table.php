d1d2c0e7ec9061347e3beb47273bfa36eed5a872
DESKTOP-PC7OFR2\omid-cl-pc <o.shabani@hotmail.com>
2024-11-26 15:29:01 +0330

    remove fortify condition in migration create

--- admin-panel/database/migrations/2024_04_30_004510_create_admin_table.php
+++ admin-panel/database/migrations/2024_04_30_004510_create_admin_table.php
@@ -24,9 +24,7 @@ return new class extends Migration
             $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')->nullable();
-            }
+            $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->boolean('is_active')->default(true);
             $table->enum('gender', ['male', 'female']);
             $table->string('avatar')->nullable();
