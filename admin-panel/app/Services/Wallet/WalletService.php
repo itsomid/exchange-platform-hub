8886624d0da08336a622f8ed1203ac1e77395a45
Mehdi Rajabi <mehdints@gmail.com>
2024-12-16 13:33:02 +0330

    feat: merge WalletService

--- admin-panel/app/Services/Wallet/WalletService.php
+++ admin-panel/app/Services/Wallet/WalletService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Wallet;
 
 use App\Enums\BalanceOperationEnum;
+use App\Models\User;
 use App\Models\Wallet;
 use App\Services\Wallet\DTO\UpdateBalanceRequestDTO;
 use Illuminate\Support\Facades\DB;
@@ -10,6 +11,31 @@ use Throwable;
 
 class WalletService
 {
+    /**
+     * Calculate the total assets value for a user's wallets.
+     *
+     * @param  \App\Models\User  $user
+     * @return float
+     */
+    public function totalAssets(User $user)
+    {
+        // Initialize the total assets value
+        $totalAssetsValue = 0;
+
+        // Loop through each wallet and calculate its value
+        foreach ($user->wallets as $wallet) {
+            // Get the current market price for the wallet's currency
+            $market = $wallet->currency->baseMarkets->first(); // Assuming you have a relationship in the Currency model
+
+            $currencyPrice = $market ? $market->activeExchangePrices->price : 1;
+
+            $totalAssetsValue += $wallet->balance * $currencyPrice;
+//
+        }
+
+        return $totalAssetsValue;
+    }
+
     public function updateBalance(UpdateBalanceRequestDTO $requestDTO): bool
     {
 
