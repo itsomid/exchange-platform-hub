5951212aac0fed61ffff1bbe5b0d78cb543f80a8
Mehdi Rajabi <mehdints@gmail.com>
2024-12-29 13:12:34 +0330

    refactor: add buy prefix

--- api-service/routes/api_v1.php
+++ api-service/routes/api_v1.php
@@ -39,6 +39,8 @@ Route::prefix('/portfolio')->group(function () {
 Route::prefix('/otc')->group(function () {
     //get-markets
     Route::get('/markets', [\App\Http\Controllers\V1\OTC\MarketController::class, 'lists'])->name('otc.markets');
-    //get bitexroom available coins
-    Route::get('/bitexroom-available-balance', [\App\Http\Controllers\V1\OTC\MarketController::class, 'bitexroomAvailableBalance'])->name('otc.bitexroom-available-balance');
+    Route::prefix('/buy')->group(function () {
+        //get bitexroom available coins
+        Route::get('/bitexroom-available-balance', [\App\Http\Controllers\V1\OTC\MarketController::class, 'bitexroomAvailableBalance'])->name('otc.bitexroom-available-balance');
+    });
 });
