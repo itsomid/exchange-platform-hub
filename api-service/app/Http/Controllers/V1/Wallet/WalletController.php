cf80259c9f047b770dcd9abfc295ae6270034522
Mehdi Rajabi <mehdints@gmail.com>
2025-01-01 14:27:25 +0330

    Revert "feat: Remove logic add pending deposit"
    This reverts commit a3fd7352f6b3794ebb25f3a7d86a2361d3a945cd.

--- api-service/app/Http/Controllers/V1/Wallet/WalletController.php
+++ api-service/app/Http/Controllers/V1/Wallet/WalletController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\V1\Wallet\GetOneWalletRequest;
 use App\Http\Resources\V1\Wallet\CoinAddressResource;
 use App\Http\Resources\V1\Wallet\GetOneWalletResource;
 use App\Http\Resources\V1\Wallet\WalletListsCollection;
+use App\Services\Wallet\DepositService;
+use App\Services\Wallet\DTO\Deposit\AddPendingDepositRequestDTO;
 use App\Services\Wallet\DTO\Wallet\GenerateAddressRequestDTO;
 use App\Services\Wallet\DTO\Wallet\GetOneWalletRequestDTO;
 use App\Services\Wallet\DTO\Wallet\WalletListsRequestDTO;
@@ -19,7 +21,7 @@ use Throwable;
 
 class WalletController extends Controller
 {
-    public function __construct(private readonly WalletService $service) {}
+    public function __construct(private readonly WalletService $service, private readonly DepositService $depositService) {}
 
     /**
      * @OA\Post(
@@ -72,6 +74,16 @@ class WalletController extends Controller
                     ->setChainSymbol($validated['chain'])
             );
 
+            //Insert Pending Deposit
+            $this->depositService->addPendingDeposit(
+                resolve(AddPendingDepositRequestDTO::class)
+                    ->setUserId($userId)
+                    ->setCurrencySymbol($validated['currency'])
+                    ->setCurrencyChain($validated['chain'])
+                    ->setPublicKey($res->getAddress())
+                    ->setExpirationDate($expirationDate = now()->addMinutes(config('bitexroom.deposit_watching_per_minutes')))
+            );
+
             DB::commit();
         } catch (Throwable $exception) {
             DB::rollBack();
@@ -83,7 +95,7 @@ class WalletController extends Controller
         }
 
         return response([
-            'data' => new CoinAddressResource($res),
+            'data' => new CoinAddressResource($res, $expirationDate),
         ]);
     }
 
@@ -139,20 +151,16 @@ class WalletController extends Controller
      *     summary="Get Wallet Balance",
      *     tags={"Wallet"},
      *     security={{"sanctum": {}}},
-     *
      *     @OA\Parameter(
      *         name="currencySymbol",
      *         in="path",
      *         description="Currency symbol to fetch the wallet details.",
      *         required=true,
-     *
      *         @OA\Schema(type="string", example="BTC")
      *     ),
-     *
      *     @OA\Response(
      *         response=200,
      *         description="Wallet details retrieved successfully.",
-     *
      *         @OA\JsonContent(ref="#/components/schemas/GetOneWalletResponse")
      *     ),
      * )
