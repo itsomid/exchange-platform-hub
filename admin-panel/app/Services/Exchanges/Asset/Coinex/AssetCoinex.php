1fd8536a701cf3931794fb6052bbd554758b5dcd
Mehdi Rajabi <mehdints@gmail.com>
2025-01-10 12:41:31 +0330

    feat: implement class to get balance from coinex

---
+++ admin-panel/app/Services/Exchanges/Asset/Coinex/AssetCoinex.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Exchanges\Asset\Coinex;
+
+use App\Services\Exchanges\Asset\Coinex\Authentication\MethodEnum;
+use App\Services\Exchanges\Asset\Contract\AssetInterface;
+use App\Services\Exchanges\Asset\DTO\BalanceResponseDTO;
+
+class AssetCoinex implements AssetInterface
+{
+    public function getBalance(): array
+    {
+        $response = CoinexRequest::send(MethodEnum::GET, "/v2/assets/spot/balance");
+
+        return array_map(function ($item){
+            return resolve(BalanceResponseDTO::class)
+                ->setCcy($item['ccy'])
+                ->setFrozen($item['frozen'])
+                ->setAvailable($item['available']);
+        }, $response->json('data'));
+    }
+}
