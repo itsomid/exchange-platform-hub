cac9bce7aa1c5fdab0e1a1583d82d4ede2d6d5f4
Mehdi Rajabi <mehdints@gmail.com>
2025-01-10 09:58:36 +0330

    fix: fix condition

--- api-service/app/Infrastructure/HDWallet/Wallet.php
+++ api-service/app/Infrastructure/HDWallet/Wallet.php
@@ -27,10 +27,11 @@ class Wallet
         //Already exists
         if ($response->badRequest()) {
             $response = Http::get(HDWallet::getBaseUrl()."/api/v1/wallet/{$blockchainName}/{$userId}");
+
             return $response->json('address');
         }
 
-        if (! $response->ok() || ! $response->created()) {
+        if (! ($response->ok() && $response->created())) {
             report($response->body());
             throw new HDDWalletServerError;
         }
