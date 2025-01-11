1347ed263d5f62a506d91fe52a99fcf3a2ccd980
Mehdi Rajabi <mehdints@gmail.com>
2025-01-11 19:33:35 +0330

    fix: fix usdt balance

--- api-service/app/Services/Wallet/WalletService.php
+++ api-service/app/Services/Wallet/WalletService.php
@@ -177,11 +177,11 @@ class WalletService
             ->setLockedBalance($wallet->locked_balance ?? 0)
             ->setUsdtBalance(
                 $wallet && $wallet->exchangePrice ?
-                    bcmul($wallet->exchangePrice->price, $wallet->balance, 8) : 0
+                    bcmul($wallet->exchangePrice->price, $wallet->balance, 8) : $wallet->balance
             )
             ->setUsdtLockedBalance(
                 $wallet && $wallet->exchangePrice ?
-                    bcmul($wallet->exchangePrice->price, $wallet->locked_balance, 8) : 0
+                    bcmul($wallet->exchangePrice->price, $wallet->locked_balance, 8) : $wallet->locked_balance
             );
     }
 }
