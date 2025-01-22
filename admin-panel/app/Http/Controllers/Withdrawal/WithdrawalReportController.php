3906daa0e487f0ae3803603cd991c0846300e988
DESKTOP-PC7OFR2\omid-cl-pc <o.shabani@hotmail.com>
2025-01-22 20:13:05 +0330

    implement withdraw report

--- admin-panel/app/Http/Controllers/Withdrawal/WithdrawalReportController.php
+++ admin-panel/app/Http/Controllers/Withdrawal/WithdrawalReportController.php
@@ -3,12 +3,78 @@
 namespace App\Http\Controllers\Withdrawal;
 
 use App\Http\Controllers\Controller;
+use App\Models\Currency;
+use App\Models\Withdrawal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Morilog\Jalali\Jalalian;
 
 class WithdrawalReportController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('dashboard.report.withdrawal');
+        $currencies = Currency::all();
+        $chartData = [];
+
+        if (!$request->from_date || !$request->to_date) {
+
+            return view('dashboard.report.withdrawal', [
+                'currencies' => $currencies,
+                'chartData' => $chartData
+            ]);
+        }
+
+        if ($request->from_date xor $request->to_date) {
+            return redirect()->route('dashboard.report.withdrawal')->withErrors(['from_date' => 'یکی از تاریخ ها نمیتواند خالی باشد']);
+        }
+
+        $from_date = \App\Helpers\DateFormatter::convertPersianToCarbonDate($request->from_date);
+        $to_date = \App\Helpers\DateFormatter::convertPersianToCarbonDate($request->to_date);
+        $today = Carbon::now();
+
+
+        // Parse dates and set times accordingly, if provided
+        // Example: "2025-01-01" -> From: 2025-01-01 00:00:00, To: 2025-01-01 23:59:59
+
+        $withdrawals = Withdrawal::selectRaw('currency_symbol, SUM(amount) as total_amount, COUNT(id) as total_transactions, DATE(created_at) as date')
+            ->where('currency_symbol', $request->currency_symbol)
+            ->when($from_date, function ($query) use ($from_date) {
+                $query->where('created_at', '>=', $from_date);
+            })
+            ->when($to_date, function ($query) use ($to_date) {
+                $query->where('created_at', '<=', $to_date);
+            })
+            ->groupBy('date', 'currency_symbol')
+            ->get();
+
+
+
+
+        foreach ($withdrawals as $withdrawal) {
+            $chartData[$withdrawal->date] = $withdrawal->total_amount;
+        }
+
+        $time_span = ($from_date || $to_date)
+            ? (int)Carbon::parse($from_date)->diffInDays($to_date) + 1
+            : 7;
+
+        $dates = [];
+        for ($i = 0; $i <= $time_span; $i++) {
+            $date = $today->copy()->subDays($i)->toDateString();
+            $dates[] = Jalalian::forge($date)->format('m/d');
+            if (!array_key_exists($date, $chartData)) {
+                $chartData[$date] = 0;
+            }
+        }
+        ksort($chartData);
+//        dd($dates , $chartData) ;
+        return view('dashboard.report.withdrawal', [
+            'withdrawals'=>$withdrawals,
+            'currencies' => $currencies,
+            'dates' => array_reverse($dates),
+            'chartData' => $chartData,
+        ]);
+
     }
 }
--- admin-panel/database/seeders/WithdrawalSeeder.php
+++ admin-panel/database/seeders/WithdrawalSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Wallet;
 
 
 use App\Services\Withdrawal\WithdrawalService;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class WithdrawalSeeder extends Seeder
@@ -15,29 +16,41 @@ class WithdrawalSeeder extends Seeder
      * Run the database seeds.
      */
     private WithdrawalService $withdrawalService;
-    private string $currencySymbol = 'BTC';
-    private string $currencyChain = 'BTC';
-    private string $address = 'bc1qsl4egjdl8s3mw822mmakvzsup7sedkd4n5755d';
+
+    private array $currencies = [
+        ['symbol' => 'BTC', 'chain' => 'BTC', 'address' => 'bc1qsl4egjdl8s3mw822mmakvzsup7sedkd4n5755d'],
+        ['symbol' => 'ETH', 'chain' => 'ETH', 'address' => '0x1234567890abcdef1234567890abcdef12345678'],
+        ['symbol' => 'TRX', 'chain' => 'TRX', 'address' => 'T1234567890abcdef1234567890abcdef12345678'],
+        ['symbol' => 'DOGE', 'chain' => 'DOGE', 'address' => 'D1234567890abcdef1234567890abcdef12345678'],
+        ['symbol' => 'BNB', 'chain' => 'BSC', 'address' => 'bnb1qsl4egjdl8s3mw822mmakvzsup7sedkd4n5755d'],
+        ['symbol' => 'USDT', 'chain' => 'ERC20', 'address' => '0xabcdefabcdefabcdefabcdefabcdefabcdef'],
+    ];
     public function __construct()
     {
         $this->withdrawalService = app(WithdrawalService::class);
     }
-
     public function run(): void
     {
+        $startDate = now()->subDays(5);
 
-        $this->processWithdrawals(0.1, 0, 3, false);
-        $this->processWithdrawals(0.05, 3, 3, true);
-        $this->processWithdrawals(0.2, 6, 3, false);
+        foreach ($this->currencies as $currency) {
+            for ($i = 1; $i < 6; $i++) {
+                $date = $startDate->copy()->addDays($i);
+
+                $this->processWithdrawals($this->generateRealisticBalance($currency['symbol']), 0, 3, false, $currency, $date);
+                $this->processWithdrawals($this->generateRealisticBalance($currency['symbol']), 3, 3, true, $currency, $date);
+//                $this->processWithdrawals(0.2, 6, 3, false, $currency, $date);
+            }
+        }
     }
 
-    private function processWithdrawals(float $totalAmount, int $skip, int $limit, bool $confirm): void
+    private function processWithdrawals(float $totalAmount, int $skip, int $limit, bool $confirm, array $currency, Carbon $date): void
     {
         $users = User::where('id', '>', 1)->skip($skip)->take($limit)->get();
 
         foreach ($users as $user) {
             $wallet = Wallet::firstOrCreate(
-                ['user_id' => $user->id, 'currency_symbol' => $this->currencySymbol],
+                ['user_id' => $user->id, 'currency_symbol' => $currency['symbol']],
                 ['balance' => 1.0, 'locked_balance' => 0]
             );
 
@@ -45,23 +58,50 @@ class WithdrawalSeeder extends Seeder
                 $withdrawal = $this->withdrawalService->createWithdrawal(
                     userId: $user->id,
                     walletId: $wallet->id,
-                    currencyChain: $this->currencyChain,
-                    currencySymbol: $this->currencySymbol,
+                    currencyChain: $currency['chain'],
+                    currencySymbol: $currency['symbol'],
                     totalAmount: $totalAmount,
-                    address: $this->address,
-                    description: 'Test withdrawal for seeding'
+                    address: $currency['address'],
+                    description: 'Test withdrawal for seeding',
+                    date: $date,
                 );
 
                 if ($confirm) {
-                    $transactionHash = '44dbec29398e9844694d317928281ff36b873d93c07ab18bcfbb7b098a91523a' . rand(1, 100);
+                    $transactionHash = '44dbec29398e9844694d317928281ff36b873d93c07ab18bcfbb7b098a91523a' . rand(1, 9999);
                     $this->withdrawalService->confirmWithdrawal($withdrawal->id, $wallet->id, $transactionHash);
-                    echo "Withdrawal created and confirmed for User ID: {$user->id}, Hash: {$transactionHash}\n";
+                    echo "Withdrawal created and confirmed for User ID: {$user->id}, Currency: {$currency['symbol']}, Hash: {$transactionHash}\n";
                 } else {
-                    echo "Withdrawal created for User ID: {$user->id}\n";
+                    echo "Withdrawal created for User ID: {$user->id}, Currency: {$currency['symbol']}\n";
                 }
             } catch (\Exception $e) {
-                echo "Error processing withdrawal for User ID {$user->id}: " . $e->getMessage() . "\n";
+                echo "Error processing withdrawal for User ID {$user->id}, Currency: {$currency['symbol']}: " . $e->getMessage() . "\n";
             }
         }
+    }
+
+    private function generateRealisticBalance(string $currency)
+    {
+        switch (strtoupper($currency)) {
+            case 'BTC':
+                return $this->randomFloat(0.001, 0.5, 8);
+            case 'ETH':
+                return $this->randomFloat(0.001, 1, 8);
+            case 'DOGE':
+                return $this->randomFloat(10, 1000, 2);
+            case 'BNB':
+                return $this->randomFloat(0.5, 1, 8);
+            case 'USDT':
+                return $this->randomFloat(50, 10000, 2);
+            case 'TRX':
+                return $this->randomFloat(10, 1000, 2);
+            default:
+                return $this->randomFloat(0, 1000, 8); // Default range for other currencies
+        }
+    }
+
+    private function randomFloat(float $min, float $max, int $decimals): float
+    {
+        $scale = pow(10, $decimals);
+        return mt_rand($min * $scale, $max * $scale) / $scale;
     }
 }
