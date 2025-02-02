38ee1b8c2ee09a43cd90e29af42b56f3f360c0c1
DESKTOP-PC7OFR2\omid-cl-pc <o.shabani@hotmail.com>
2025-02-02 16:36:18 +0330

    add hd wallet bnetwork fee to withdrawal

--- admin-panel/database/migrations/2024_12_20_114020_create_withdrawals_table.php
+++ admin-panel/database/migrations/2024_12_20_114020_create_withdrawals_table.php
@@ -15,12 +15,13 @@ return new class extends Migration
             $table->id(); // Primary key
             $table->unsignedBigInteger('user_id'); // Foreign key
             $table->unsignedBigInteger('admin_id')->nullable(); // Foreign key
-            $table->string('currency_chain'); // Blockchain (e.g., Ethereum, Binance Smart Chain)
+            $table->string(\'currency_chain'); // Blockchain (e.g., Ethereum, Binance Smart Chain)
             $table->string('currency_symbol'); // Token/Currency Symbol (e.g., BTC, ETH)
             $table->decimal('amount', 20, 8); // Withdrawal amount
             $table->decimal('total_fee', 20, 8)->default(0); // Withdraw Total fee (optional)
             $table->decimal('exchange_fee', 20, 8)->default(0); // Exchange fee (optional)
             $table->decimal('network_fee', 20, 8)->default(0); // Exchange fee (optional)
+            $table->decimal('hd_wallet_network_fee', 20, 8)->default(0)->nullable(); // Exchange fee (optional)
             $table->decimal('usdt_value', 20, 8)->nullable();
             $table->string('address')->nullable(); // Withdrawal address
             $table->string('transaction_hash', 96)->nullable()->unique();
