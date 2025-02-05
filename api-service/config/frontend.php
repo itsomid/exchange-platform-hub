d5c0e8acba692bdabd2fab4c70d554fbd2c12bdc
DESKTOP-PC7OFR2\omid-cl-pc <o.shabani@hotmail.com>
2025-02-05 11:18:53 +0330

    fix forntend url issue

--- api-service/config/frontend.php
+++ api-service/config/frontend.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'reset-password-link' => env('RESET_PASSWORD_LINK', 'http://localhost/reset-password?token=%s'),
-    'email-verification-link' => env('RESET_PASSWORD_LINK', 'http://localhost/email-verification?token=%s'),
-    'reset-two-factor-verification-link' => env('RESET_TWO_FACTOR_LINK', 'http://localhost/reset-two-factor?token=%s'),
+    'reset-password-link' => env('RESET_PASSWORD_LINK', 'http://localhost/auth/reset-password?token=%s'),
+    'email-verification-link' => env('EMAIL_VERIFICATION_LINK', 'http://localhost/auth/email-verification?token=%s'),
+    'reset-two-factor-verification-link' => env('RESET_TWO_FACTOR_LINK', 'http://localhost/auth/reset-two-factor?token=%s'),
 ];
