88857240961198f82815801dbe650db28524ead7
Mehdi Rajabi <mehdints@gmail.com>
2025-02-05 16:03:04 +0330

    feat: Queue notification and email

--- api-service/app/Notifications/ResetPasswordNotification.php
+++ api-service/app/Notifications/ResetPasswordNotification.php
@@ -5,9 +5,10 @@ namespace App\Notifications;
 use App\Mail\ResetPasswordMail;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class ResetPasswordNotification extends Notification
+class ResetPasswordNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
