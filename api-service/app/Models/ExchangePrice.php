63f9aa8bf4071040d02515759076425f4a2d3be4
DESKTOP-PC7OFR2\omid-cl-pc <o.shabani@hotmail.com>
2025-02-08 16:36:56 +0330

    fix market price

--- api-service/app/Models/ExchangePrice.php
+++ api-service/app/Models/ExchangePrice.php
@@ -27,11 +27,11 @@ class ExchangePrice extends Model
 
     public function getSellPriceAttribute(): string
     {
-        return Math::mul($this->price, ($this->exchange_profit_sell + 1));
+        return Math::add(Math::mul($this->price , ( $this->exchange_profit_buy / 100 )) , $this->price);
     }
 
     public function getBuyPriceAttribute(): string
     {
-        return Math::mul($this->price, ($this->exchange_profit_sell + 1));
+        return Math::add(Math::mul($this->price , ( $this->exchange_profit_sell / 100 )) , $this->price);
     }
 }
