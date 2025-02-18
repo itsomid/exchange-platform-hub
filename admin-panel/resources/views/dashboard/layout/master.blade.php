380a9574cce029dfb0642fa37ef6da7b1daa6cf7
OMID-PC\Omid <o.shabani@hotmail.com>
2025-02-18 09:55:43 +0330

    notif modal problem

--- admin-panel/resources/views/dashboard/layout/master.blade.php
+++ admin-panel/resources/views/dashboard/layout/master.blade.php
@@ -89,7 +89,7 @@
 </div>
 <!-- / Layout wrapper -->
 
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+{{--<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>--}}
 <!-- Core JS -->
 @include('dashboard.layout.sections.scripts')
 
