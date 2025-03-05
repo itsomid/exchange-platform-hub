7de06517efaea1fe8e5321bbdac23f6fc48c0d14
Mehdi Rajabi <mehdints@gmail.com>
2025-03-05 13:34:29 +0330

    feat: create spot migration

--- admin-panel/database/migrations/2025_03_04_121659_create_spot_orders_table.php
+++ admin-panel/database/migrations/2025_03_04_121659_create_spot_orders_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Market;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +15,25 @@ return new class extends Migration
     {
         Schema::create('spot_orders', function (Blueprint $table) {
             $table->id();
-            //TODO
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Market::class)->constrained();
+            $table->string('order_type'); //buy, sell
+            $table->string('order_kind'); //market, limit
+            $table->decimal('quantity', 18, 8);
+            $table->decimal('price', 18, 8);
+            $table->string('status');
+            $table->timestamps();
+        });
+
+        Schema::create('sport_trade', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('buy_order_id');
+            $table->unsignedInteger('sell_order_id');
+
+            $table->decimal('quantity', 18, 8);
+            $table->decimal('price', 18, 8);
+
+            $table->unique(['buy_order_id', 'sell_order_id']);
             $table->timestamps();
         });
     }
