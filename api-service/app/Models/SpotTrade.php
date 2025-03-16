533e72ea9b2ef134875f48e534a9e3b3a35e7c12
Mehdi Rajabi <mehdints@gmail.com>
2025-03-16 17:07:22 +0330

    feat: add transactions

--- api-service/app/Models/SpotTrade.php
+++ api-service/app/Models/SpotTrade.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
- * @property int $id
+ * @property int    $id
+ * @property string $quantity
  */
 class SpotTrade extends Model
 {
     protected $fillable = [
         'maker_order_id', 'taker_order_id', 'quantity', 'price',
     ];
+
+    public function commission(): BelongsTo
+    {
+        return $this->belongsTo(TradingCommission::class);
+    }
 }
