f6277f08090462230dbdbad3ac92d00868f08edc
Mehdi Rajabi <mehdints@gmail.com>
2025-04-04 19:13:04 +0330

    feat: dispatch immediately

--- admin-panel/app/Services/Socket/CoinExSocketService.php
+++ admin-panel/app/Services/Socket/CoinExSocketService.php
@@ -187,7 +187,7 @@ class CoinExSocketService
                 'timestamp' => now()->timestamp,
             ]));
 
-            MarketUpdated::dispatch($this->marketIds[$baseCurrency], [
+            MarketUpdated::dispatchSync($this->marketIds[$baseCurrency], [
                 'low' => $data['low'],
                 'high' => $data['high'],
                 'last' => $data['last'],
