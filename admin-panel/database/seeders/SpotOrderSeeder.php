7caefc0f74368dbf39f8d620a1f960d8445ea0ce
Mehdi Rajabi <mehdints@gmail.com>
2025-04-07 16:40:25 +0330

    fix: Use same market_id

--- admin-panel/database/seeders/SpotOrderSeeder.php
+++ admin-panel/database/seeders/SpotOrderSeeder.php
@@ -15,14 +15,21 @@ class SpotOrderSeeder extends Seeder
         SpotOrder::factory()->count(20)->create();
 
         // You could also create a sample trade manually to match valid maker/taker
-        $maker = SpotOrder::factory()->create(['type' => 'limit', 'price' => 15000]);
-        $taker = SpotOrder::factory()->create(['type' => 'market', 'price' => null]);
+        $maker = SpotOrder::factory()->state([
+            'type' => 'limit',
+            'price' => 15000
+        ])->create();
+        $taker = SpotOrder::factory()->state([
+            'type' => 'market',
+            'price' => null,
+            'market_id' => $maker->market_id
+        ])->create();
 
-        $trade = SpotTrade::factory()->create([
+        $trade = SpotTrade::factory()->state([
             'maker_order_id' => $maker->id,
             'taker_order_id' => $taker->id,
             'market_id' => $maker->market_id,
-        ]);
+        ])->create();
 
         TradingCommission::factory()->create([
             'spot_trade_id' => $trade->id,
