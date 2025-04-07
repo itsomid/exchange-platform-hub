b29dec0a8f26f9e1b5bc4b9397b805babd0752e9
OMID-PC\Omid <o.shabani@hotmail.com>
2025-04-07 16:05:38 +0330

    add filled amount to ref exchange bought report

--- admin-panel/resources/views/dashboard/exchange/ref_exchange/bought-history.blade.php
+++ admin-panel/resources/views/dashboard/exchange/ref_exchange/bought-history.blade.php
@@ -62,7 +62,8 @@
                         <th>ID</th>
                         <th>شماره سفارش</th>
                         <th>بازار</th>
-                        <th>مقدار</th>
+                        <th>مقدار درخواست</th>
+                        <th>مقدار خریداری شده<br>(filled_amount)</th>
                         <th>فی</th>
                         <th>میانگین قیمت (USDT)</th>
                         <th>ارزش (USDT)</th>
@@ -92,7 +93,8 @@
                                 </div>
                             </td>
 
-                            <td class="font-number">{{formatNumberTrimZeros($transaction->amount)}} ({{$transaction->currency->symbol}})</td>
+                            <td class="font-number">{{$transaction->amount}} ({{$transaction->currency->symbol}})</td>
+                            <td class="font-number">{{$transaction->filled_amount}} ({{$transaction->currency->symbol}})</td>
                             <td class="font-number">
 
                                 @if($transaction->fee > 0)
