3b2b1819d168b9c1de71281719669992a399d394
OMID-PC\Omid <o.shabani@hotmail.com>
2025-04-07 16:09:08 +0330

    change text

--- admin-panel/resources/views/dashboard/exchange/ref_exchange/bought-history.blade.php
+++ admin-panel/resources/views/dashboard/exchange/ref_exchange/bought-history.blade.php
@@ -62,8 +62,8 @@
                         <th>ID</th>
                         <th>شماره سفارش</th>
                         <th>بازار</th>
-                        <th>مقدار درخواست</th>
-                        <th>مقدار خریداری شده<br>(filled_amount)</th>
+                        <th>مقدار درخواستی</th>
+                        <th>مقدار پر شده<br>(filled_amount)</th>
                         <th>فی</th>
                         <th>میانگین قیمت (USDT)</th>
                         <th>ارزش (USDT)</th>
@@ -93,8 +93,8 @@
                                 </div>
                             </td>
 
-                            <td class="font-number">{{$transaction->amount}} ({{$transaction->currency->symbol}})</td>
-                            <td class="font-number">{{$transaction->filled_amount}} ({{$transaction->currency->symbol}})</td>
+                            <td class="font-number">{{formatNumberTrimZeros($transaction->amount)}} ({{$transaction->currency->symbol}})</td>
+                            <td class="font-number">{{formatNumberTrimZeros($transaction->filled_amount ?? 'N/A')}} ({{$transaction->currency->symbol}})</td>
                             <td class="font-number">
 
                                 @if($transaction->fee > 0)
