1034f9566966763d813bd81397194d9f6128becd
OMID-PC\Omid <o.shabani@hotmail.com>
2025-05-06 15:11:36 +0330

    fix exchange withdrawal fee input issue

--- admin-panel/resources/views/dashboard/exchange/currency/edit-chains.blade.php
+++ admin-panel/resources/views/dashboard/exchange/currency/edit-chains.blade.php
@@ -87,7 +87,7 @@
                                     <div class="col-md-3 ">
                                         <div class="form-group">
                                             <label class="form-label" for="exchange_withdrawal_fee_{{$chain->id}}">فی صرافی برای برداشت (واحد)</label>
-                                            <input  type="number" name="chains[{{$chain->id}}][exchange_withdrawal_fee]" id="exchange_withdrawal_fee_{{$chain->id}}" class="form-control font-number"
+                                            <input  type="text" name="chains[{{$chain->id}}][exchange_withdrawal_fee]" id="exchange_withdrawal_fee_{{$chain->id}}" class="form-control font-number"
                                                     placeholder="فی صرافیی را وارد کنید." value="{{formatNumber($chain->exchange_withdrawal_fee,$currency->precision)}}" required>
                                             @error('exchange_withdrawal_fee')
                                             <small class="text-danger">{{$message}}</small>
