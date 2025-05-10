f903341783d87c118c1f96e9fe1967c51ee6c1d9
OMID-PC\Omid <o.shabani@hotmail.com>
2025-05-10 17:32:58 +0330

    add new exception to coinex error

--- admin-panel/app/Services/Exchanges/Enums/CoinexWithdrawalError.php
+++ admin-panel/app/Services/Exchanges/Enums/CoinexWithdrawalError.php
@@ -7,6 +7,7 @@ enum CoinexWithdrawalError: int
     case Below_Minimum_Amount = 11022;
     case Address_White_List = 11050;
     case Asset_Insufficient = 11008;
+    case Exceeding_Withdrawal_Decimal_Limit = 11029;
 
     public static function mapErrorToResponse(CoinexWithdrawalError $errorEnum): string
     {
@@ -14,6 +15,7 @@ enum CoinexWithdrawalError: int
             CoinexWithdrawalError::Below_Minimum_Amount => 'مقدار تجمیع تراکنش پایین تر از حد مجاز است.',
             CoinexWithdrawalError::Address_White_List => 'آدرس ولت در وایت لیست نمی باشد.',
             CoinexWithdrawalError::Asset_Insufficient => 'عدم موجودی کافی (فی برداشت) برای انجام فرآیند تجمیع.',
+            CoinexWithdrawalError::Exceeding_Withdrawal_Decimal_Limit => 'تعداد اعشار مقدار برداشت بیشتر از حد مجار است.',
             default => 'خطای ناشناخته، لطفا دوباره تلاش کنید.',
         };
     }
