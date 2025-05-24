89f3f687d81dccdcb05f3861151bb82ba5429b2f
OMID-PC\Omid <o.shabani@hotmail.com>
2025-05-24 23:16:08 +0330

    add exchange name to accounting collection

--- api-service/app/Http/Resources/Accounting/V1/TransactionResource.php
+++ api-service/app/Http/Resources/Accounting/V1/TransactionResource.php
@@ -14,19 +14,7 @@ class TransactionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        if ($this->spot_trade_id) {
-            $price = $this->spotTrade?->MakerOrder->price;
-        } elseif ($this->otc_order_id) {
-            if($this->wallet->currency_symbol === 'USDT'){
-                $price = "1";
-            }
-            elseif ($this->wallet->currency_symbol === 'CET') {
-                $price = $this->coin_price;
-            }
-            else {
-                $price = $this->otcOrder?->price;
-            }
-        }
+
 
         return [
             'id' => $this->id,
@@ -41,6 +29,7 @@ class TransactionResource extends JsonResource
             'balance' => $this->balance,
             'type' => $this->type,
             'subtype' => $this->subtype,
+            'ref_exchange' => $this->exchange?->slug,
             'status' => $this->status,
             'description' => $this->description,
             'created_at' => $this->created_at,
