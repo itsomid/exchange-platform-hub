c0ac182e722836944e9d3259b9a1f992c80c9c09
OMID-PC\Omid <o.shabani@hotmail.com>
2025-06-07 20:55:46 +0330

    run db backup

--- admin-panel/app/Console/Kernel.php
+++ admin-panel/app/Console/Kernel.php
@@ -17,6 +17,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('exchange:fetch-min-otc-amount coinex')->dailyAt('01:00');
         $schedule->command('exchange:fetch-deposit-withdrawal-config coinex')->dailyAt('02:00');
         $schedule->command('bitexroom:transfer-to-hot-wallet')->everyFiveMinutes();
+        $schedule->command('backup:clean')->daily()->at('03:00');
+        $schedule->command('backup:run')->daily()->at('04:00');
     }
 
     /**
