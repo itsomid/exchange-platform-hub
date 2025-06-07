59e337b677066c8d1e8c82b88ebd15c7eb4b60b7
OMID-PC\Omid <o.shabani@hotmail.com>
2025-06-07 20:59:52 +0330

    add needed package

--- admin-panel/config/backup.php
+++ admin-panel/config/backup.php
@@ -200,7 +200,7 @@ return [
             \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['mail'],
             \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
             \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => [App\Notifications\CustomBackupWasSuccessful::class],
+            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => ['mail'],
             \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => ['mail'],
             \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => ['mail'],
         ],
