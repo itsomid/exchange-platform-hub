e0035b30c2258c2e5f4f2e89217077501789dd20
OMID-PC\Omid <o.shabani@hotmail.com>
2025-07-07 14:13:16 +0330

    implement sort and filter to stocks

---
+++ admin-panel/app/Filters/StockContractFilter/SortByQuantity.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filters\StockContractFilter;
+
+use App\Filters\FilterContract;
+
+class SortByQuantity implements FilterContract
+{
+    protected $query;
+
+    public function __construct($query)
+    {
+        $this->query = $query;
+    }
+
+    public function handle($value = null): void
+    {
+        if ($value == 'desc') {
+            $this->query->orderBy('quantity', $value);
+        }
+
+        if ($value == 'asc') {
+            $this->query->orderBy('quantity', $value);
+        }
+    }
+}
