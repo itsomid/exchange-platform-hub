16ce526bdba27ac4f5f11e2bf228680e394d806a
OMID-PC\Omid <o.shabani@hotmail.com>
2025-07-09 12:36:02 +0330

    add CONTRACTS_FILE_BASE_URL foir admin panel

--- admin-panel/config/bitexroom.php
+++ admin-panel/config/bitexroom.php
@@ -5,7 +5,7 @@ return [
         'base_url' => env('BITEX_API_SERVICE_BASE_URL', 'http://nginx-api'),
     ],
     'contracts' => [
-        'base_url' => env('CONTRACTS_BASE_URL', 'http://127.0.0.1:8001'),
+        'base_url' => env('CONTRACTS_FILE_BASE_URL', 'http://127.0.0.1:8001'),
     ],
     'user_id' => env('BITEXROOM_USER_ID', 1),
     'scale_precision' => 8,
