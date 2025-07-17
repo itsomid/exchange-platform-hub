338d6d4c63714d34ec314af3e7a86cb0b721ad44
OMID-PC\Omid <o.shabani@hotmail.com>
2025-07-17 12:54:21 +0330

    show cancel reason in otc order list

--- admin-panel/resources/views/dashboard/otc_order/index.blade.php
+++ admin-panel/resources/views/dashboard/otc_order/index.blade.php
@@ -362,6 +362,40 @@
                                    data-bs-target="#otc-{{$order->id}}">
                                     <i class="fa-light fa-memo-circle-info fa-xl"></i>
                                 </a>
+                                @if($order->status === \App\Enums\OTCOrderStatusEnum::CANCELED)
+                                <a href="#" class="btn btn-icon btn-text-secondary" data-bs-toggle="modal"
+                                   data-bs-target="#otc-description-{{$order->id}}">
+                                    <i class="fa-regular fa-eye fa-xl"></i>
+                                </a>
+                                @endif
+                                <div class="modal fade" id="otc-description-{{$order->id}}" tabindex="-1"
+                                     aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header" dir="ltr">
+                                                <h5 class="modal-title font-number">OTC Order
+                                                    #{{$order->id}}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div
+                                                    class="d-flex flex-column flex-sm-row align-items-sm-center justify-content-between border-bottom pb-4 mb-4">
+
+                                                    <h6 class="m-0 mb-2 mb-md-0 me-12">توضیحات صرافی مرجع (دلیل لغو)</h6>
+                                                    <div class="text-wrap font-number">
+                                                        {{$order->ref_exchange_description}}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-label-secondary"
+                                                        data-bs-dismiss="modal">بستن
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="modal fade" id="otc-{{$order->id}}" tabindex="-1" aria-model="true"
                                      role="dialog">
                                     <div class="modal-dialog modal-xl" role="document">
