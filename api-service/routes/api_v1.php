d6c4ec8fe9e8b5dd26a25ef004ace1bf28ba3160
OMID-PC\Omid <o.shabani@hotmail.com>
2025-10-20 19:13:31 +0330

    implement deposit transactions route

--- api-service/routes/api_v1.php
+++ api-service/routes/api_v1.php
@@ -43,6 +43,7 @@ Route::prefix('/wallets')->group(function () {
     Route::get('/lists', [WalletController::class, 'lists'])->name('wallets.lists');
     Route::get('/value-usdt', [WalletController::class, 'assetsUSDTValue'])->name('wallets.value-usdt');
 
+    Route::get('/deposits', [\App\Http\Controllers\V1\Wallet\DepositController::class, 'lists'])->name('wallets.deposits');
     Route::get('/withdrawals', [\App\Http\Controllers\V1\Wallet\WithdrawController::class, 'lists'])->name('wallets.withdrawals');
     Route::post('/withdrawal', [\App\Http\Controllers\V1\Wallet\WithdrawController::class, '__invoke'])->name('wallets.withdraw')->middleware([\App\Http\Middleware\FinancialWithdrawalBlockMiddleware::class]);
 
@@ -55,10 +56,7 @@ Route::prefix('saved-addresses')->group(function () {
     Route::post('/addresses', [\App\Http\Controllers\V1\Wallet\SavedAddressController::class, 'save']);
     Route::delete('/addresses/{savedAddressId}', [\App\Http\Controllers\V1\Wallet\SavedAddressController::class, 'delete']);
 });
-// Transaction
-Route::prefix('transactions')->group(function () {
-    Route::get('/all-deposit-withdraw', [\App\Http\Controllers\V1\Transaction\TransactionController::class, 'allDepositWithdraw'])->name('transactions.all-deposit-withdraw');
-});
+
 // Portfolio
 Route::prefix('/portfolio')->group(function () {
     Route::get('/last-week', [\App\Http\Controllers\V1\Wallet\PortfolioController::class, 'getPortfolioLastWeek'])->name('portfolio.get-portfolio-last-week');
