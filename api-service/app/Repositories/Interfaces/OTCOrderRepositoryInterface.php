b0326ffa08c22aee9c8e16f94b58829689a0a1c2
OMID-PC\Omid <o.shabani@hotmail.com>
2025-12-14 14:20:59 +0330

    implement pagination for otc order

--- api-service/app/Repositories/Interfaces/OTCOrderRepositoryInterface.php
+++ api-service/app/Repositories/Interfaces/OTCOrderRepositoryInterface.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Interfaces;
 
 use App\Models\OTCOrder;
 use App\Repositories\DTO\OTCOrder\CreateOTCOrderRequestDTO;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 interface OTCOrderRepositoryInterface
@@ -13,4 +14,6 @@ interface OTCOrderRepositoryInterface
     public function lists(int $userId, array $queryString): Collection;
 
     public function getOneById(int $id): OTCOrder;
+
+    public function listsPaginated(int $userId, array $queryString, int $page = 1, int $perPage = 10): LengthAwarePaginator;
 }
