8a4bf0bf426e23139c86b72df6372383c470ee08
OMID-PC\Omid <o.shabani@hotmail.com>
2026-03-30 15:32:31 +0330

    improve footer

--- admin-panel/resources/views/dashboard/layout/footer.blade.php
+++ admin-panel/resources/views/dashboard/layout/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="content-footer footer bg-footer-theme">
-    <div class="container-xxl">
-        <div class="footer-container d-flex align-items-center justify-content-end py-2 flex-md-row flex-column ">
+    <div class="container-fluid flex-grow-1">
+        <div class="footer-container d-flex align-items-center justify-content-start py-2 flex-md-row flex-column ">
             <div class="footer">
-                © 2025 Made With ❤️ by Bitexroom
+                © {{ now()->year }} Made With ❤️ by Bitexroom
             </div>
         </div>
     </div>
