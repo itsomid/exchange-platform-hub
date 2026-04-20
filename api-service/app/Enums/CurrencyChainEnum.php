8df33a3cc8f10e14b39790b9804dae3ae8fd2c30
OMID-PC\Omid <o.shabani@hotmail.com>
2026-04-21 00:00:12 +0330

    add avalanche network to api

--- api-service/app/Enums/CurrencyChainEnum.php
+++ api-service/app/Enums/CurrencyChainEnum.php
@@ -5,18 +5,14 @@ namespace App\Enums;
 enum CurrencyChainEnum: string
 {
     case BTC = 'BTC';         // Bitcoin blockchain
-
     case ETH = 'ETH';         // Ethereum Mainnet
-    case TRX = 'TRX';         //Tron Mainnet
     case ERC20 = 'ERC20';     // Ethereum ERC-20 token standard
     case BEP20 = 'BEP20';     // Binance Smart Chain (BSC) BEP-20 token standard
     case TRC20 = 'TRC20';     // TRON TRC-20 token standard
     case BSC = 'BSC';         // Binance Smart Chain (Native chain)
     case DOGE = 'DOGE';       // Dogecoin blockchain
-    case SOL = 'SOL';         // Solana blockchain
-    case POLYGON = 'POLYGON'; // Polygon blockchain
-    case LTC = 'LTC';         // Litecoin blockchain
-    case AVAX = 'AVAX';       // Avalanche blockchain
+    case POLYGON = 'POLYGON'; // Polygon blockchain  
     case ARBITRUM = 'ARBITRUM'; // Arbitrum One (L2)
     case OPTIMISM = 'OPTIMISM'; // Optimism (L2)
+    case AVALANCHE = 'AVALANCHE';       // Avalanche blockchain
 }
