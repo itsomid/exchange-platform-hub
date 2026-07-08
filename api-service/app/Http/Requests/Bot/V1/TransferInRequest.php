e43674c70a83dc7e344bbf0c9115bd99ea5fd00b
OMID-PC\Omid <o.shabani@hotmail.com>
2026-07-08 16:12:26 +0330

    Update minimum deposit validation in TransferInRequest to use dynamic value from BotGlobalSettings
    - Modified the validation rule for the 'amount' field to set the minimum deposit dynamically based on the current BotGlobalSettings, enhancing flexibility in deposit requirements.

--- api-service/app/Http/Requests/Bot/V1/TransferInRequest.php
+++ api-service/app/Http/Requests/Bot/V1/TransferInRequest.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Requests\Bot\V1;
 
+use App\Models\Bot\BotGlobalSettings;
 use Illuminate\Foundation\Http\FormRequest;
 
 class TransferInRequest extends FormRequest
 {
     public function rules(): array
     {
+        $minDeposit = BotGlobalSettings::current()->min_deposit_usdt;
+
         return [
-            'amount' => ['required', 'numeric', 'min:20'],
+            'amount' => ['required', 'numeric', 'min:'.$minDeposit],
         ];
     }
 }
